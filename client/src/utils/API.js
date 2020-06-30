1ac602c82b4c2e9528d5702e28568eea986871c0
Patton Parham <patton.parham@verizon.net>
2020-06-30 13:22:22 -0500

    delete button issue solved

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,7 +8,7 @@ export default {
         return axios.get("/api/sounds" + id);
      },
      deleteSound: function(id){
-         return axios.delete("api/sounds" + id);
+         return axios.delete("/api/sounds/" + id);
      },
     saveSound: function(soundData){
         return axios.post("/api/sounds", soundData);
